600c2b6be79d37db52e1d1d29e505a8bb268a32b
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-28 16:55:59 +0800

    feat: support fullscreen

--- scripts/static-server.js
+++ scripts/static-server.js
@@ -22,7 +22,7 @@ app.use(async (ctx, next) => {
 app.use(home);
 
 const _server = app.listen(6515);
-console.log(`server start => http://localhost:6515/test/test.html`);
+console.log(`server start => http://localhost:6515/test/demo.html`);
 
 export default {
   server: _server,
---
+++ src/o-code.js
@@ -0,0 +1,10 @@
+export const type = $.COMP;
+
+export const temp = "./temp.html";
+
+export default async ({ load }) => {
+  return {
+    tag: "o-code",
+    data: {},
+  };
+};
