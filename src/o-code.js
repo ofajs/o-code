523d8690f4ddf4c1ed2d507cbada3775e744b382
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-29 15:10:08 +0800

    feat: change to iconify

--- src/o-code.js
+++ src/o-code.js
@@ -1,10 +1,12 @@
 export const type = $.COMP;
 
-export const temp = "./temp.html";
+export const temp = "./o-code-temp.html";
 
 export default async ({ load }) => {
   return {
     tag: "o-code",
-    data: {},
+    data: {
+      activeName: "Files",
+    },
   };
 };
