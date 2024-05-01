fc238ce99fa9afbb015e25ee455eb8501d3274ac
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-05-01 14:08:27 +0800

    feat: add list fill

--- src/o-code.js
+++ src/o-code.js
@@ -7,6 +7,33 @@ export default async ({ load }) => {
     tag: "o-code",
     data: {
       activeName: "Files",
+      list: [
+        {
+          name: "o-code",
+          list: [
+            {
+              name: "demo.html",
+            },
+            {
+              name: "code.js",
+            },
+            {
+              name: "code2.css",
+            },
+            {
+              name: "test-dir",
+              list: [
+                {
+                  name: "code3.json",
+                },
+                {
+                  name: "code4.md",
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
   };
 };
