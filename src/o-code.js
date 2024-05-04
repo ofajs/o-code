f81e06799515ff0776e1f69c2a8e820484f86779
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-05-04 15:45:06 +0800

    refactor: change selected file code

--- src/o-code.js
+++ src/o-code.js
@@ -2,43 +2,55 @@ export const type = $.COMP;
 
 export const temp = "./o-code-temp.html";
 
+const testlist = [
+  {
+    name: "o-code",
+    path: "o-code",
+    folder: 1,
+    list: [
+      {
+        name: "demo.html",
+        path: "o-code/demo.html",
+      },
+      {
+        name: "code.js",
+        path: "o-code/code.js",
+      },
+      {
+        name: "code2.css",
+        path: "o-code/code2.css",
+      },
+      {
+        name: "test-dir",
+        folder: 1,
+        list: [
+          {
+            name: "code3.json",
+            path: "o-code/test-dir/code3.json",
+          },
+          {
+            name: "code4.md",
+            path: "o-code/test-dir/code4.md",
+          },
+        ],
+      },
+    ],
+  },
+];
+
 export default async ({ load }) => {
   return {
     tag: "o-code",
     data: {
       activeName: "Files",
-      list: [
-        {
-          name: "o-code",
-          folder: 1,
-          list: [
-            {
-              name: "demo.html",
-            },
-            {
-              name: "code.js",
-            },
-            {
-              name: "code2.css",
-              selected: 1,
-            },
-            {
-              name: "test-dir",
-              folder: 1,
-              list: [
-                {
-                  name: "code3.json",
-                },
-                {
-                  name: "code4.md",
-                },
-              ],
-            },
-          ],
-        },
-      ],
+      list: [...testlist],
+      tabs: [],
+      selectedFilePath: "",
     },
     proto: {
+      clickTab(data) {
+        this.selectedFilePath = data.path;
+      },
       dblclickItem(data, event) {
         event.stopPropagation();
 
@@ -52,16 +64,19 @@ export default async ({ load }) => {
       clickItem(data, event) {
         event.stopPropagation();
 
-        if (data.selected) {
-          return;
-        }
-
-        // 将原来激活的项目都清空
-        this.shadow
-          .all("ocode-file-item[selected]")
-          .forEach(($el) => ($el.__item.$data.selected = null));
-
         data.selected = 1;
+
+        if (!data.list) {
+          this.selectedFilePath = data.path;
+
+          // 不在tabs上就直接添加
+          if (!this.tabs.find((e) => e.path === this.selectedFilePath)) {
+            this.tabs.push({
+              name: data.name,
+              path: data.path,
+            });
+          }
+        }
       },
     },
   };
